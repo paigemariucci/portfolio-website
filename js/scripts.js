5803f8f44f5f755e835990bd34e15db39ef34a29
paigemariucci <paige.mariucci@gmail.com>
2023-02-21 15:54:13 -0800

    Update js file for mobile toggle menu & smooth scroll interactions

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
 console.log("Welcome to my portfolio website!")
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+    } else {
+    x.className = 'navtoggle';
+    }
+}
+
+$('#goTop').on('click', function(e){
+    $("html, body").animate({scrollTop: $("#top").offset().top}, 500);
+    return false;
+    });
